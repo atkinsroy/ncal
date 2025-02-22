7f12c6ea01f488957ed79b0d4426531420bd70b7
Roy Atkins <roy.atkins@hpe.com>
2025-02-22 15:12:54 +1100

    Added comment about Microsoft Terminal support for unicode character sets

--- ncal.psm1
+++ ncal.psm1
@@ -825,6 +825,10 @@ function Get-NCalendar {
         character sets are both available and are displayed correctly. With other consoles, like Visual Studio Code,
         the ISE and the default PowerShell console, some fonts might not display correctly and with extended unicode
         character sets, calendars may appear misaligned.
+
+        Note: Some versions of Windows Terminal have been found to misalign unicode character sets. For example, 
+        version 1.21.2911.0 (Nov 2024) works well, whilst version 1.22.10352.0 (Feb 2025) has reintroduced unicode 
+        character misalignment.
     .PARAMETER Month
         Specifies the required month. This must be specified as a number 0..13. An 'f' (forward by one year) or a
         'p' (previous by one year) suffix can also be appended to the month number.
@@ -1315,6 +1319,10 @@ function Get-Calendar {
         character sets are both available and are displayed correctly. With other consoles, like Visual Studio Code,
         the ISE and the default PowerShell console, some fonts might not display correctly and with extended unicode
         character sets, calendars may appear misaligned.
+
+        Note: Some versions of Windows Terminal have been found to misalign unicode character sets. For example, 
+        version 1.21.2911.0 (Nov 2024) works well, whilst version 1.22.10352.0 (Feb 2025) has reintroduced unicode 
+        character misalignment.
     .PARAMETER Month
         Specifies the required month. This must be specified as a number 0..13. An 'f' (forward by one year) or a
         'p'  (previous year) suffix can also be appended to the month number.
